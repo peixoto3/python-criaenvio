532244fdfdf433f66c825bc36283c61a4e21f5ab
Peixoto <gpeixoto3@gmail.com>
2020-08-18 11:59:35 -0300

    removendo teste duplicado.

--- tests/test_contato.py
+++ tests/test_contato.py
@@ -53,32 +53,6 @@ class TestContatoCliente(unittest.TestCase):
 
         self.assertEqual(url_esperada, url)
 
-    @patch('requests.get')
-    def test_obter_contato_por_id(self, mock_obter_contato_por_id):
-        contato_esperado = {
-            'data': {
-                'id': 'aBGoa',
-                'nome': 'Guilherme Peixoto',
-                'email': 'gpeixoto3@gmail.com',
-                'qualidade': '5',
-                'ativo': True,
-                'descadastrado': False,
-                'sexo': 'm',
-                'dataNascimento': '1994-05-03',
-                'dataCadastro': '2020-08-10 18:58:16.133884'
-            },
-            'embeds': [
-                'grupos',
-                'campos'
-            ]
-        }
-
-        mock_obter_contato_por_id.return_value.json.return_value = contato_esperado
-
-        contato = self.cliente_contato.obter_por_id('id_teste')
-
-        self.assertEqual(contato_esperado, contato)
-
     @patch('requests.post')
     def test_criar_contato(self, mock_criar_contato):
         nome = 'Guilherme Peixoto'
